f8401910d6ba784c791cd849c353a29eb15f6bc3
PREETI11082000 <p19.satsangi@gmail.com>
2023-10-11 15:30:53 +0530

    api

--- src/Components/Products/index.js
+++ src/Components/Products/index.js
@@ -1,8 +1,22 @@
-import { useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import './_products.scss';
-import productSlice from '../../store/Products';
+import { getProducts } from '../../store/Products/productAction';
+import { addCartItem } from '../../store/Cart/cartSlice';
+
 const Products = ()=>{
-    const productData = useSelector(productSlice.getInitialState)
+    const productData = useSelector(state => state.pr.products);
+    const cart = useSelector(state => state.cr);
+    const dispatch = useDispatch();
+
+    useEffect(()=>{
+        dispatch(getProducts());
+    },[])
+
+    const addToCart = (itemData)=>{
+        dispatch(addCartItem(itemData));
+    }
+    console.log(cart);
 
     return(
         <div className='products-container'>
@@ -11,17 +25,27 @@ const Products = ()=>{
                     return(
                         <div className='mx-5 p-3 product-card'>
                             <div className='product-image-container'>
-                                <img src={require('../../assets/images/shop/'+product.img)}/>
+                                <img src={require('../../assets/images/shop/'+product.product_img)}/>
                             </div>
                             <div className='product-info'>
-                                <h5> <a href='#'>{product.pName}</a> </h5>
+                                <h5> <a href='#'>{product.product_name}</a> </h5>
                                 <p className='product-price'> ${product.price} </p>
                                 <div className='product-rating'>
-                                <i className='fa fa-star'/>
-                                <i className='fa fa-star'/>
-                                <i className='fa fa-star'/>
-                                <i className='fa fa-star'/>
-                                <i className='fa fa-star'/>
+                                    <i className='fa fa-star'/>
+                                    <i className='fa fa-star'/>
+                                    <i className='fa fa-star'/>
+                                    <i className='fa fa-star'/>
+                                    <i className='fa fa-star'/>
+                                </div>
+                            </div>
+                            <div className='my-3' onClick={()=>addToCart(product)}>
+                                <div className='cart-button'>
+                                    <div className='cart-icon-container'>
+                                        <i className='fa fa-shopping-cart mx-4'/>
+                                    </div>
+                                    <div className='cart-text-container mx-3'>
+                                        <p> Add to Cart </p>
+                                    </div>
                                 </div>
                             </div>
                         </div>
